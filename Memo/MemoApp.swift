8d8b5213984d061ae6926ccace3e139a883ba03a
RewEPIC <hongtae1011@gmail.com>
2024-08-10 02:24:48 +0700

    Initial Commit

---
+++ Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by Duangcharoen on 10/8/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
