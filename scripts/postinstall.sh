7387d8a1d056b4cf8f4a412166e20806fa3d39f7
Duangcharoen <duangcharoen.siwa@kmutt.ac.th>
2025-02-23 01:22:45 +0700

    Test postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,4 +3,22 @@ echo "Running postinstall script for react-native-reanimated-skeleton to support
 
 # Replace react-native-linear-gradient with expo-linear-gradient and LinearGradient import to { LinearGradient } with the
 # react-native-reanimated-skeleton node_module package
-find ./node_modules/react-native-reanimated-skeleton -type f -exec sed -i '' -e 's/import LinearGradient/import { LinearGradient }/g' -e 's/react-native-linear-gradient/expo-linear-gradient/g' {} +
+# find ./node_modules/react-native-reanimated-skeleton -type f -exec sed -i '' -e 's/import LinearGradient/import { LinearGradient }/g' -e 's/react-native-linear-gradient/expo-linear-gradient/g' {} +
+
+# Ensure the directory exists
+if [ -d "./node_modules/react-native-reanimated-skeleton" ]; then
+  echo "Modifying react-native-reanimated-skeleton..."
+
+  # Detect OS: macOS or Linux
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS (Darwin)
+    find ./node_modules/react-native-reanimated-skeleton -type f -exec sed -i '' -e 's/import LinearGradient/import { LinearGradient }/g' -e 's/react-native-linear-gradient/expo-linear-gradient/g' {} +
+  else
+    # Linux (GitHub Actions)
+    find ./node_modules/react-native-reanimated-skeleton -type f -exec sed -i -e 's/import LinearGradient/import { LinearGradient }/g' -e 's/react-native-linear-gradient/expo-linear-gradient/g' {} +
+  fi
+
+  echo "Modification completed."
+else
+  echo "react-native-reanimated-skeleton not found, skipping..."
+fi
